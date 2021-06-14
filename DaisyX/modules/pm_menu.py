a9fbc07ad9daaaaee323f46e74f42e611bb6e2f6
gplkavin <81810902+gplkavin@users.noreply.github.com>
2021-06-14 17:44:22 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -45,16 +45,16 @@ def help_markup(modules):
 
 
 STICKERS = (
-    "CAACAgUAAxkBAAJOGmBeli95P073FKVkgc4esfKE4UlXAAIOAgACyavAVkbLMIidWYdyHgQ",
-    "CAACAgUAAxkBAAJOG2BeljABwlCfwzHT1gzyiciBri6_AAIsAgACXBPBVgpGQRz-1qmlHgQ",
-    "CAACAgUAAxkBAAJOHGBeljOJ35CQNnkpnVcgRoHuJX6DAAL3AQACN8TBVm1PIART01cWHgQ",
-    "CAACAgUAAxkBAAJOHWBeljXW9QzYQ51gpCjHZHCF5Ui6AAJ7AgAC3zDBVo2xenp7JYhAHgQ",
-    "CAACAgUAAxkBAAJOHmBeljjU0_FT_QpdUUJBqVUC0nfJAAKYAgACJ_jBVvntHY_8WF27HgQ",
-    "CAACAgUAAxkBAAJOH2BeljrV68mPLu8_6n4edT20Q3IQAAJ9AgACq3LBVmLuZuNPlvkfHgQ",
-    "CAACAgUAAxkBAAJOIGBeljttuniUPykRtzkSZj3SRwKJAAI7AgACNm_BVp8TCkE6ZqCoHgQ",
-    "CAACAgUAAxkBAAJOIWBelj-P_2vtVqtkF2OMlVN3M0N4AAK3AQACSm3BVkXF2voraS2tHgQ",
-    "CAACAgUAAxkBAAJOImBelkJxUBm2rL1iPfMZfk-_9DaOAALrAgAC4T3BVniopXQVsZ4KHgQ",
-    "CAACAgUAAxkBAAJOI2BelkMO0AX_wtAc7hUZz1NixuMlAAKEAwACY4TAViVuNLTBmmkgHgQ",
+    "CAACAgUAAx0CR81OLwADkmDHRpqSzfnQ05u0BPir0DFzxvT6AAI\_AwACESE5VqzPbbOgkD7LHwQ",
+    "CAACAgUAAx0CR81OLwADkWDHRpi56Ewhp0de5lmWm\_oJFSsfAALfBAACUxs5Vn3w88s7Aoo9HwQ",
+    "CAACAgUAAx0CR81OLwADkGDHRpcMi8aiilRLArr47h3nDUANAAIsBAACMQk5VpKxncAT7RNuHwQ",
+    "CAACAgUAAx0CR81OLwADj2DHRpVYme1WLGT54L0AAeAdcWkEAQACugIAAn20QVbrlIaBlnYCRR8E",
+    "CAACAgUAAx0CR81OLwADjmDHRpTPF-5OR3oYgvHcDGLeawpEAAInAgAC7WZBVqR9DQQlV4t1HwQ",
+    "CAACAgUAAx0CR81OLwADjWDHRpPuf07G3iOP01wRMvo1VMudAAIeAwAC4O84Viw7eITnI4ECHwQ",
+    "CAACAgUAAx0CR81OLwADjGDHRpFyW\_uj6vF\_KQdEPZrRPAABNwACdwMAAjCNOFZzbtWbb6Prnh8E",
+    "CAACAgUAAx0CR81OLwADi2DHRpATKtTA01S1kUArX8MFVAKTAAJ2AgACarA4VjH-WbVEn03wHwQ",
+    "CAACAgUAAx0CR81OLwADimDHRo4xfAcAAdeeBFM0gyIBbR-4RwACEAMAAs0vOVYzAAEYR5\_idOkfBA",
+    "CAACAgUAAx0CR81OLwADiWDHRoygjxgmiK5zDpe1umyIBpojAAJuAwACWH45Vj-IRfWzvHsEHwQ",
 )
 
 
@@ -135,7 +135,7 @@ async def help_cmd(message, strings):
 async def help_cmd_g(message, strings):
     text = strings["btn_group_help"]
     button = InlineKeyboardMarkup().add(
-        InlineKeyboardButton(text=text, url="https://t.me/DaisyXBOT?start")
+        InlineKeyboardButton(text=text, url="https://t.me/gpl_managerv2bot?start")
     )
     await message.reply(strings["help_header"], reply_markup=button)
 
